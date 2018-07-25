38ee3eb9316e72529acb426c546d54a858168c59
jnizet <jb@ninja-squad.com>
2018-07-25 10:20:06 +0200

    chore: add script to harvest data

---
+++ scripts/harvest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+DATADIR="$BASEDIR/../data"
+
+mkdir -p /tmp/rare/resources
+cp $DATADIR/*.json.gz /tmp/rare/resources
+
+gzip -d -f /tmp/rare/resources/*.json.gz
+
+curl -i -X POST -u rare:f01a7031fc17 http://localhost:8080/api/harvests
