56a6f21a2e6440cd5fb4f545adb7d62d03c59c38
jnizet <jb@ninja-squad.com>
2018-08-14 17:55:28 +0200

    fix: add missing index filter to support deep linking
    fix #20

---
+++ backend/src/test/java/fr/inra/urgi/rare/IndexFilterTest.java
@@ -0,0 +1,65 @@
+package fr.inra.urgi.rare;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import fr.inra.urgi.rare.config.SecurityConfig;
+import fr.inra.urgi.rare.dao.GeneticResourceDao;
+import fr.inra.urgi.rare.search.SearchController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Unit tests for {@link IndexFilter}
+ * @author JB Nizet
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = SearchController.class)
+@Import(SecurityConfig.class)
+class IndexFilterTest {
+    @MockBean
+    private GeneticResourceDao mockGeneticResourceDao;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldForwardToIndexForAngularUrl() throws Exception {
+        mockMvc.perform(get("/search?query=vitis"))
+               .andExpect(forwardedUrl("/index.html"));
+    }
+
+    @Test
+    void shouldNotForwardToIndexWhenNotGet() throws Exception {
+        mockMvc.perform(post("/search?query=vitis"))
+               .andExpect(forwardedUrl(null));
+    }
+
+    @Test
+    void shouldNotForwardToIndexWhenStaticResource() throws Exception {
+        List<String> urls = Arrays.asList("/index.html",
+                                          "/script.js",
+                                          "/style.css",
+                                          "/image.gif",
+                                          "/icon.ico",
+                                          "/image.png",
+                                          "/image.jpg",
+                                          "/font.woff",
+                                          "/font.ttf",
+                                          "/actuator/info");
+        for (String url : urls) {
+            mockMvc.perform(get(url))
+                   .andExpect(forwardedUrl(null));
+        }
+    }
+}
