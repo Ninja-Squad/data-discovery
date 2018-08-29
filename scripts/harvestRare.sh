c1c1b7b97eef6def6ca3635fc9023bee300f4914
Raphaël Flores <raphael.flores@versailles.inra.fr>
2018-08-29 22:16:55 +0200

    Fix variable names.

--- scripts/harvestRare.sh
+++ scripts/harvestRare.sh
@@ -2,11 +2,11 @@
 
 BASEDIR=$(dirname "$0")
 DATADIR="$BASEDIR/../data/rare"
-es-host=$1
+app_host=$1
 
 mkdir -p /tmp/rare/resources
 cp $DATADIR/*.json.gz /tmp/rare/resources
 
 gzip -d -f /tmp/rare/resources/*.json.gz
 
-curl -i -X POST -u rare:f01a7031fc17 http://${es-host}:8080/rare/api/harvests
+curl -i -X POST -u rare:f01a7031fc17 http://${app_host}:8080/rare/api/harvests
--- scripts/harvestWheatis.sh
+++ scripts/harvestWheatis.sh
@@ -2,10 +2,11 @@
 
 BASEDIR=$(dirname "$0")
 DATADIR="$BASEDIR/../data/wheatis"
+app_host=$1
 
 mkdir -p /tmp/wheatis/resources
 cp $DATADIR/*.json.gz /tmp/wheatis/resources
 
 gzip -d -f /tmp/wheatis/resources/*.json.gz
 
-curl -i -X POST -u rare:f01a7031fc17 http://localhost:8080/wheatis/api/harvests
+curl -i -X POST -u rare:f01a7031fc17 http://${app_host}:8080/wheatis/api/harvests
