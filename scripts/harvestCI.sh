04f3b26ef876b111cb75af038c26b89b4492aa9c
Raphaël Flores <raphael.flores@versailles.inra.fr>
2018-08-30 18:37:59 +0200

    Create job for deploying WheatIS. Create script for RARe & WheatIS apps indexing and deployment.

---
+++ scripts/harvestCI.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# set -x
+BASEDIR=$(dirname "$0")
+APP_HOST=$1
+APP_PORT=$2
+APP_NAME=$3
+ENV=$4
+
+EXP_PARAMS=4
+[ ! $# -eq $EXP_PARAMS ] && {
+    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected $EXP_PARAMS params: 1:APP_HOST ; 2:APP_PORT ; 3:APP_NAME ; 4:ENV"
+    exit 4
+}
+
+DATADIR="$BASEDIR/../data/$APP_NAME"
+INDEX_DATA_DIR="/tmp/$APP_NAME-$ENV/resources"
+
+echo "Creating directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
+cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
+gzip -d -f $INDEX_DATA_DIR/*.json.gz && \
+curl -f -i -X POST -u rare:f01a7031fc17 http://${APP_HOST}:8080/$APP_NAME/api/harvests
