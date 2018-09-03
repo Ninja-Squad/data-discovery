35fdb0ab1c2b9b6d1989d7e8df9a87e5524fd3a2
Raphaël Flores <raphael.flores@versailles.inra.fr>
2018-09-04 00:55:23 +0200

    Handle error codes.

--- scripts/createIndexAndAliases4CI.sh
+++ scripts/createIndexAndAliases4CI.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
 ES_HOST=$1
 APP_NAME=$2
 ENV=$3
 
-[ -z "$3" ] && {
-    echo "ERROR: missing at least 1 parameter. Expected 3 params: 1:ES_HOST ; 2:APP_NAME ; 3:ENV"
+
+EXP_PARAMS=3
+[ ! $# -eq $EXP_PARAMS ] && {
+    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected "\
+	"$EXP_PARAMS params: 1:ES_HOST ; 2:APP_NAME ; 3:ENV"
     exit 4
 }
-
-BASEDIR=$(dirname "$0")
 
 # NOTE: If the settings are changed here, they must also be changed the same way in
 # backend/src/test/resources/fr/inra/urgi/rare/dao/settings.json for the unit tests
 
-curl -X PUT "${ES_HOST}:9200/${APP_NAME}-${ENV}-resource-physical-index" -H 'Content-Type: application/json' -d"
+curl -si -X PUT "${ES_HOST}:9200/${APP_NAME}-${ENV}-resource-physical-index"\
+ -H 'Content-Type: application/json' -d"
 {
     \"aliases\" : {
         \"${APP_NAME}-${ENV}-resource-index\" : {},
@@ -35,3 +38,4 @@ curl -X PUT "${ES_HOST}:9200/${APP_NAME}-${ENV}-resource-physical-index" -H 'Con
     }
 }
 "
+echo
