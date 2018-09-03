171f6d0cc7893050137c6c97f7257b0611bd20a0
Raphaël Flores <raphael.flores@versailles.inra.fr>
2018-09-04 00:55:23 +0200

    Handle error codes.

--- scripts/harvestCI.sh
+++ scripts/harvestCI.sh
@@ -8,7 +8,8 @@ ENV=$4
 
 EXP_PARAMS=4
 [ ! $# -eq $EXP_PARAMS ] && {
-    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected $EXP_PARAMS params: 1:APP_HOST ; 2:APP_PORT ; 3:APP_NAME ; 4:ENV"
+    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected "\
+	"$EXP_PARAMS params: 1:APP_HOST ; 2:APP_PORT ; 3:APP_NAME ; 4:ENV"
     exit 4
 }
 
@@ -16,6 +17,15 @@ DATADIR="$BASEDIR/../data/$APP_NAME"
 INDEX_DATA_DIR="/tmp/$APP_NAME-$ENV/resources"
 
 echo "Creating directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
-cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
-gzip -d -f $INDEX_DATA_DIR/*.json.gz && \
-curl -f -i -X POST -u rare:f01a7031fc17 http://${APP_HOST}:$APP_PORT/$APP_NAME/api/harvests
+{
+	cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
+	gzip -d -f $INDEX_DATA_DIR/*.json.gz && \
+	curl -f -i -X POST -u rare:f01a7031fc17 \
+	"http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}/api/harvests"
+} || {
+	code=$?
+	echo -e "A problem occured (code=$code) when trying to index data from\n\t"\
+		"$INDEX_DATA_DIR \n on app:\n\t"\
+		"http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}"
+	exit $code
+}
