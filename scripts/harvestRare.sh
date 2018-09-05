bd34041e9d80ecd9d8a52cc60f2ca22e519561a5
Raphaël Flores <raphael.flores@inra.fr>
2018-09-05 17:07:41 +0200

    Refactor scripts to use only parameterized scripts, but keeping simple scripts for usability. Update configuration according to default port differing for rare (8080) and wheatis (8081) in dev mode.

--- scripts/harvestRare.sh
+++ scripts/harvestRare.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
+# delegates to parameterized script
 BASEDIR=$(dirname "$0")
-DATADIR="$BASEDIR/../data/rare"
-app_host=$1
 
-mkdir -p /tmp/rare/resources
-cp $DATADIR/*.json.gz /tmp/rare/resources
-
-gzip -d -f /tmp/rare/resources/*.json.gz
-
-curl -i -X POST -u rare:f01a7031fc17 http://${app_host}:8080/rare/api/harvests
+sh $BASEDIR/harvestCI.sh localhost 8080 rare dev
--- scripts/harvestWheatis.sh
+++ scripts/harvestWheatis.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
+# delegates to parameterized script
 BASEDIR=$(dirname "$0")
-DATADIR="$BASEDIR/../data/wheatis"
-app_host=$1
 
-mkdir -p /tmp/wheatis/resources
-cp $DATADIR/*.json.gz /tmp/wheatis/resources
-
-gzip -d -f /tmp/wheatis/resources/*.json.gz
-
-curl -i -X POST -u rare:f01a7031fc17 http://${app_host}:8080/wheatis/api/harvests
+sh $BASEDIR/harvestCI.sh localhost 8081 wheatis dev
