29ef156a618f7231a55648c3b7fa9972daddb687
Raphaël Flores <raphael.flores@inra.fr>
2018-11-21 16:18:45 +0100

    Fix e2e configuration (rare-dev default endpoint) and add firefox to chrome.

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -1,19 +1,29 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
-
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
+  multiCapabilities: [
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ["--headless", "--disable-gpu"]
+      }
+    },
+    {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ["--headless"]
+      }
+    }
+  ],
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
@@ -23,6 +33,6 @@ exports.config = {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
   }
 };
--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to rare-frontend!');
+    expect(page.getParagraphText()).toEqual('RARe\n' +
+      'Ressources Agronomiques pour la Recherche');
   });
 });
