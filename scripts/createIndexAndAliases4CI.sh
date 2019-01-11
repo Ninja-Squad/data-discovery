5d492c7978dcfcadb3c97f3d359641ddff768d81
Raphaël Flores <raphael.flores@inra.fr>
2019-01-11 16:12:25 +0100

    Simplify index creation by reusing existing settings and mappings. GNP-5408.

--- scripts/createIndexAndAliases4CI.sh
+++ scripts/createIndexAndAliases4CI.sh
@@ -2,6 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 ES_HOST=$1
+ES_PORT=9200
 APP_NAME=$2
 ENV=$3
 
@@ -13,32 +14,24 @@ EXP_PARAMS=3
     exit 4
 }
 
-# NOTE: If the settings are changed here, they must also be changed the same way in
-# backend/src/test/resources/fr/inra/urgi/datadiscovery/dao/settings.json for the unit tests
 
-curl -si -X PUT "${ES_HOST}:9200/${APP_NAME}-${ENV}-resource-physical-index"\
+echo -e "\nCreate index: ${APP_NAME}-${ENV}-resource-physical-index"
+curl -si -X PUT "${ES_HOST}:${ES_PORT}/${APP_NAME}-${ENV}-resource-physical-index"\
  -H 'Content-Type: application/json' -d"
 {
     \"aliases\" : {
         \"${APP_NAME}-${ENV}-resource-index\" : {},
         \"${APP_NAME}-${ENV}-resource-harvest-index\" : {}
     },
-    \"settings\": {
-        \"number_of_replicas\": 0,
-        \"number_of_shards\": 5,
-        \"refresh_interval\": \"30s\",
-        \"analysis\": {
-            \"analyzer\": {
-                \"custom_suggestion_analyzer\": {
-                    \"type\": \"custom\",
-                    \"tokenizer\": \"standard\",
-                    \"filter\": [
-                        \"lowercase\"
-                    ]
-                }
-            }
-        }
+    \"settings\":
+        $(cat ${BASEDIR}/../backend/src/test/resources/fr/inra/urgi/datadiscovery/dao/settings.json )
     }
 }
+"
+
+echo -e "\n\nApply mapping: $(ls -1 ${BASEDIR}/../backend/src/main/resources/fr/inra/urgi/datadiscovery/domain/${APP_NAME}/*.mapping.json)"
+curl -si -X PUT "${ES_HOST}:${ES_PORT}/${APP_NAME}-${ENV}-resource-physical-index/_mapping/${APP_NAME}-${ENV}-resource"\
+ -H 'Content-Type: application/json' -d"
+$(cat ${BASEDIR}/../backend/src/main/resources/fr/inra/urgi/datadiscovery/domain/${APP_NAME}/*.mapping.json)
 "
 echo
