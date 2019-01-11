7b034cff52d5ffb3a50ec626516539d375e41522
Raphaël Flores <raphael.flores@inra.fr>
2019-01-11 16:11:17 +0100

    Allow usage of parallel for decompressing files when harvesting data. GNP-5408.

--- scripts/harvestCI.sh
+++ scripts/harvestCI.sh
@@ -6,6 +6,9 @@ APP_PORT=$2
 APP_NAME=$3
 ENV=$4
 
+PARALLEL_FOUND=true
+command -v parallel > /dev/null || PARALLEL_FOUND=false
+
 EXP_PARAMS=4
 [ ! $# -eq $EXP_PARAMS ] && {
     echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected "\
@@ -16,10 +19,17 @@ EXP_PARAMS=4
 DATADIR="$BASEDIR/../data/$APP_NAME"
 INDEX_DATA_DIR="/tmp/$APP_NAME-$ENV/resources"
 
-echo "Creating directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
+echo "Creating index directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
 {
-	cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
-	gzip -d -f $INDEX_DATA_DIR/*.json.gz && \
+	echo "Decompressing data files from $DATADIR into $INDEX_DATA_DIR"
+	if [ "$PARALLEL_FOUND" == "true" ]; then
+		echo "... using GNU parallel"
+		parallel --bar "gzip -d -f -c {} > $INDEX_DATA_DIR/{/.}" ::: $DATADIR/*.json.gz
+	else
+		echo "... sequentially"
+		cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
+		gzip -d -f $INDEX_DATA_DIR/*.json.gz
+	fi
 	curl -f -i -X POST -u ${APP_NAME}:f01a7031fc17 \
 	"http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}/api/harvests"
 } || {
