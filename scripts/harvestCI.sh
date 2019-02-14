41d1cd8f9189c2b4b3d73488955ed2c35cfb24aa
Celia Michotey <celia.michotey@inra.fr>
2019-02-14 18:21:40 +0100

    Add explicit parameters and add a copy option.

--- scripts/harvestCI.sh
+++ scripts/harvestCI.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 # set -x
+
+
+help() {
+	cat <<EOF
+DESCRIPTION: 
+	Script used to index data in Data Discovery portals (RARe, WheatIS and GnpIS)
+
+USAGE:
+	$0 -host <host name> -port <port value> -app <application name> -env <environment name> -copy [-h|--help]
+
+PARAMS:
+	-host          the host name of the targeted application
+	-port          the port value of the targeted application
+	-app           the name of the targeted application: rare, wheatis or gnpis
+	-env           the environement name of the targeted application (dev, beta, prod ...)
+	-copy          to set if the data files needs to be copied on the application host
+	-h or --help   print this help
+
+EOF
+	exit 1
+}
+
 BASEDIR=$(dirname "$0")
-APP_HOST=$1
-APP_PORT=$2
-APP_NAME=$3
-ENV=$4
+APP_HOST=""
+APP_PORT=""
+APP_NAME=""
+ENV=""
+COPY_FILES=0
+
+# any params
+[ -z "$1" ] && echo && help
+
+# get params
+while [ -n "$1" ]; do
+	case $1 in
+		-h) help;shift 1;;
+		--help) help;shift 1;;
+		-host) APP_HOST=$2;shift 2;;
+		-port) APP_PORT=$2;shift 2;;
+		-app) APP_NAME=$2;shift 2;;
+		-env) ENV=$2;shift 2;;
+		-copy) COPY_FILES=1;shift 1;;
+		--) shift;break;;
+		-*) echo "Unknown option: $1" && echo && help && echo;exit 1;;
+		*) break;;
+	esac
+done
 
 PARALLEL_FOUND=true
 command -v parallel > /dev/null || PARALLEL_FOUND=false
 
 EXP_PARAMS=4
 [ ! $# -eq $EXP_PARAMS ] && {
-    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s). Expected "\
-	"$EXP_PARAMS params: 1:APP_HOST ; 2:APP_PORT ; 3:APP_NAME ; 4:ENV"
-    exit 4
+    echo "ERROR: missing $((EXP_PARAMS-$#)) parameter(s)." && help
+	exit 4
 }
 
-DATADIR="$BASEDIR/../data/$APP_NAME"
 INDEX_DATA_DIR="/tmp/$APP_NAME-$ENV/resources"
-
-echo "Creating index directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
-{
+if [ $COPY_FILES -eq 1 ]; then
+    DATADIR="$BASEDIR/../data/$APP_NAME"
+	echo "Creating index directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
 	echo "Decompressing data files from $DATADIR into $INDEX_DATA_DIR"
 	if [ "$PARALLEL_FOUND" == "true" ]; then
 		echo "... using GNU parallel"
@@ -30,12 +70,18 @@ echo "Creating index directory: $INDEX_DATA_DIR" && mkdir -p $INDEX_DATA_DIR
 		cp $DATADIR/*.json.gz $INDEX_DATA_DIR && \
 		gzip -d -f $INDEX_DATA_DIR/*.json.gz
 	fi
+else
+    echo "Decompressing data files from $INDEX_DATA_DIR"
+    gzip -d -f $INDEX_DATA_DIR/*.json.gz
+fi
+
+{
 	curl -f -i -X POST -u ${APP_NAME}:f01a7031fc17 \
 	"http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}/api/harvests"
 } || {
 	code=$?
-	echo -e "A problem occured (code=$code) when trying to index data from\n\t"\
-		"$INDEX_DATA_DIR \n on app:\n\t"\
-		"http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}"
+	echo -e "A problem occured (code=$code) when trying to index data \n"\
+		"\tfrom $INDEX_DATA_DIR\n"\
+		"\ton app http://${APP_HOST}:${APP_PORT}/${APP_NAME}-${ENV}"
 	exit $code
 }
