dda8624e760a145d707dfe9680bd9c5ae450ce26
belhouda <bilal.el-houdaigui@inra.fr>
2019-04-10 14:27:49 +0200

    changed the data dir name from gnpis to data-discovery.

--- scripts/harvestGnpis.sh
+++ scripts/harvestGnpis.sh
@@ -3,4 +3,4 @@
 # delegates to parameterized script
 BASEDIR=$(dirname "$0")
 
-sh $BASEDIR/harvestCI.sh -url http://localhost:8280/data-discovery-dev -app gnpis -env dev -copy
+sh $BASEDIR/harvestCI.sh -url http://localhost:8280/data-discovery-dev -app data-discovery -env dev -copy
