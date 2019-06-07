00c5f56a907607d1a60d7b0abc8fe30b66d07a5a
Raphaël Flores <raphael.flores@inra.fr>
2019-06-07 14:26:49 +0200

    Fix failing test.

--- frontend/src/app/help/help.component.spec.ts
+++ frontend/src/app/help/help.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { HelpComponent } from './help.component';
 
@@ -8,7 +9,8 @@ describe('HelpComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpComponent ]
+      declarations: [ HelpComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
