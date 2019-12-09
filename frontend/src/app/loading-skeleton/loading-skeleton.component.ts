d9bee1e783173564236725608f7c6f0279bbcd60
Cyril Pommier <cyril.pommier@inra.fr>
2019-12-09 19:09:47 +0100

    Improved skeleton look and feel.fix GNP-5671

--- frontend/src/app/loading-skeleton/loading-skeleton.component.ts
+++ frontend/src/app/loading-skeleton/loading-skeleton.component.ts
@@ -6,24 +6,38 @@ import { ErrorInterceptorService } from '../error-interceptor.service';
 @Component({
     selector: 'dd-loading-skeleton',
     template: `
-        <div *ngIf="loading" class="card loading">
+      <div *ngIf=" ! aggregationStyle">
+        <div *ngIf="loading" class="card skeleton-loading">
           <div class="skeleton-default skeleton-animated-background"></div>
         </div>
-        <div *ngIf="loading" class="card loading">
+        <div *ngIf="loading" class="card skeleton-loading">
           <div class="skeleton-default skeleton-animated-background"></div>
         </div>
-        <div *ngIf="loading" class="card loading">
+        <div *ngIf="loading" class="card skeleton-loading">
           <div class="skeleton-default skeleton-animated-background"></div>
         </div>
-        <div *ngIf="loading" class="card loading">
+        <div *ngIf="loading" class="card skeleton-loading">
           <div class="skeleton-default skeleton-animated-background"></div>
         </div>
+      </div>
+      <div *ngIf="aggregationStyle">
+          <div *ngIf="loading" class="card skeleton-loading-aggregation">
+              <div class="skeleton-aggregation skeleton-animated-background"></div>
+          </div>
+          <div *ngIf="loading" class="card skeleton-loading-aggregation">
+              <div class="skeleton-aggregation skeleton-animated-background"></div>
+          </div>
+          <div *ngIf="loading" class="card skeleton-loading-aggregation">
+              <div class="skeleton-aggregation skeleton-animated-background"></div>
+          </div>
+      </div>
     `,
     styleUrls: ['./loading-skeleton.component.scss']
 })
 export class LoadingSkeletonComponent implements OnInit {
 
     @Input() loading: boolean;
+    @Input() aggregationStyle = false;
 
     constructor(private errorService: ErrorInterceptorService) {
     }
