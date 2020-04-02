ecd7a14579467dd36ed13f6ef822b11fab56c20e
cexbrayat <cedric@ninja-squad.com>
2020-04-02 11:35:03 +0200

    feat: setup i18n testing

---
+++ frontend/src/app/i18n/mock-i18n.spec.ts
@@ -0,0 +1,26 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { MissingTranslationHandler, MissingTranslationHandlerParams, TranslateModule, TranslateService } from '@ngx-translate/core';
+import EN_TRANSLATIONS from './en.json';
+
+export class CustomMissingTranslationHandler implements MissingTranslationHandler {
+  handle(params: MissingTranslationHandlerParams) {
+    throw new Error(`Missing translation for key ${params.key}`);
+  }
+}
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      useDefaultLang: false,
+      missingTranslationHandler: { provide: MissingTranslationHandler, useClass: CustomMissingTranslationHandler }
+    })
+  ],
+  exports: [TranslateModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'en' }]
+})
+export class I18nTestingModule {
+  constructor(translateService: TranslateService) {
+    translateService.setTranslation('en', EN_TRANSLATIONS);
+    translateService.use('en');
+  }
+}
