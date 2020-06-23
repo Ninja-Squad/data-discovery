63989a2874a74a944fa9061867870c389aaecb8a
cexbrayat <cedric@ninja-squad.com>
2020-06-23 10:26:14 +0200

    refactor: tidy up markdown component

--- frontend/src/app/markdown-page/markdown-page.component.spec.ts
+++ frontend/src/app/markdown-page/markdown-page.component.spec.ts
@@ -1,23 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarkdownPageComponent } from './markdown-page.component';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { of } from 'rxjs';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { markedOptionsFactory } from '../app.module';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeRoute } from 'ngx-speculoos';
 
 describe('MarkdownPageComponent', () => {
   let component: MarkdownPageComponent;
   let fixture: ComponentFixture<MarkdownPageComponent>;
-  const route = ({ data: of({ mdFile: environment.helpMdFile }) } as any) as ActivatedRoute;
+  const route = fakeRoute({ data: of({ mdFile: environment.helpMdFile }) });
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MarkdownPageComponent ],
-      providers: [{ provide: ActivatedRoute, useValue: route }],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [MarkdownPageComponent],
+    imports: [HttpClientTestingModule, MarkdownModule.forRoot({
+      loader: HttpClient, // optional, only if you use [src] attribute
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory,
+        useValue: {
+          gfm: true, // default
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false
+        }
+      }
+    })],
+    providers: [{ provide: ActivatedRoute, useValue: route }]
   }));
 
   beforeEach(() => {
@@ -26,8 +42,21 @@ describe('MarkdownPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load and display the help file', () => {
+    const http = TestBed.inject(HttpTestingController);
+
+    // the markdown file is extracted from the route data by our component
     expect(component.mdFile).toEqual('assets/help.md');
+
+    // the markdown component requests the server for the file
+    // we return a fake markdown with just a title
+    http.expectOne({
+      method: 'GET',
+      url: 'assets/help.md'
+    }).flush('# Help section');
+
+    // the markdown component should render the title
+    const title = fixture.nativeElement.querySelector('h1').textContent;
+    expect(title).toBe('Help section');
   });
 });
--- frontend/src/app/markdown-page/markdown-page.component.ts
+++ frontend/src/app/markdown-page/markdown-page.component.ts
@@ -2,29 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'dd-about',
+  selector: 'dd-markdown',
   templateUrl: './markdown-page.component.html',
   styleUrls: ['./markdown-page.component.scss']
 })
 export class MarkdownPageComponent implements OnInit {
 
-  mdFile = '';
+  mdFile: string;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-      this.route.data.subscribe(
-          value => this.mdFile = value.mdFile);
-  }
-
-  onLoad(e: any) {
-    // console.log('Into onLoad');
-    // console.log(e);
-  }
-
-  onError(e: any) {
-    console.log('Got error', e);
+      this.route.data.subscribe(value => this.mdFile = value.mdFile);
   }
 
 }
