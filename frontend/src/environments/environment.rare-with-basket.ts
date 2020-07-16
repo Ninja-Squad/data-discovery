2274a93f1ff29bbf31d585bda1db964204357d5d
Raphaël Flores <raphael.flores@inra.fr>
2020-07-16 16:54:38 +0200

    Change environment file for rare-with-basket using BRC4Env examples, and links. GNP-5796

---
+++ frontend/src/environments/environment.rare-with-basket.ts
@@ -0,0 +1,33 @@
+import { RareModule } from '../app/rare/rare.module';
+
+export const environment = {
+  production: false,
+  title: 'BRC4Env - Biological Resource Centre for the Environment',
+  navbar: {
+    title: 'BRC4Env Search',
+    logoUrl: '',
+    links: [
+      { label: 'BRC4env Home', url: 'https://www.brc4env.fr/' }
+    ]
+  },
+  searchPlaceholder: 'e.g. salmo, chlamydomonas, phytomyzinae',
+  resourceModule: RareModule,
+  helpMdFile: 'assets/help.md',
+  aboutUsMdFile: 'assets/about.md',
+  joinUsMdFile: 'assets/join.md',
+  legalMentionsMdFile: 'assets/legal.md',
+  eulaMdFile: 'assets/eula.md',
+  dataProvider: 'Data providers',
+  /**
+   * Map containing the list of the aggregations and their displayed name.
+   * Should be kept in sync with the `RareAggregation` enum of the backend.
+   */
+  aggregationNames: {
+    coo: 'Pays d\'origine',
+    coc: 'Pays de collecte',
+    domain: 'Domaine',
+    material: 'Matériel',
+    taxon: 'Taxon',
+    biotope: 'Biotope'
+  }
+};
