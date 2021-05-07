f8c9ab2ec3c176c4b771728a9a7082163bb05de2
Erik Kimmel <erik.kimmel@inrae.fr>
2021-05-07 14:09:05 +0000

    Chore: update deprecated sonarqube properties and add properties needed for MR decoration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
-    id("org.sonarqube") version "2.8"
+    id("org.sonarqube") version "3.2.0"
+}
+
+sonarqube {
+    properties {
+        property ("sonar.projectKey", "urgi-is_data-discovery_AXlG7GtvPgTGgvpuDgeV")
+        property ("sonar.qualitygate.wait", false)
+    }
 }
