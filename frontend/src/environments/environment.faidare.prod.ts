d4e1c5a25564790ba6f74e044fd0789d51077a1a
cexbrayat <cedric@ninja-squad.com>
2021-06-24 10:31:12 +0200

    refactor: share common config in env files

--- frontend/src/environments/environment.faidare.prod.ts
+++ frontend/src/environments/environment.faidare.prod.ts
@@ -2,67 +2,9 @@
 // `ng build --configuration=production` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { FaidareModule } from '../app/faidare/faidare.module';
+import { environment as devEnvironment } from './environment.faidare';
 
 export const environment = {
-  production: true,
-  title: 'FAIR Data-finder for Agronomic REsearch',
-  navbar: {
-    title: 'FAIDARE',
-    logoUrl: '',
-    links: [
-      {
-        label: 'URGI',
-        url: '#',
-        subMenu: [
-          { label: 'Home', url: 'https://urgi.versailles.inra.fr' },
-          { label: 'News', url: 'https://urgi.versailles.inra.fr/About-us/News' },
-          { label: 'About us', url: 'https://urgi.versailles.inra.fr/About-us' }
-        ]
-      }
-    ],
-    // TODO should the contributor be displayed in the navbar?
-    contributor: {
-      name: 'Elixir',
-      url: 'https://elixir-europe.org/',
-      logo: 'assets/elixir_logo.png'
-    }
-  },
-  searchPlaceholder: 'Examples: yield, fhb',
-  resourceModule: FaidareModule,
-  helpMdFile: 'assets/help.md',
-  aboutUsMdFile: 'assets/about.md',
-  joinUsMdFile: 'assets/join.md',
-  legalMentionsMdFile: 'assets/legal.md',
-  eulaMdFile: 'assets/eula.md', // TODO does not exist
-  newsMdFile: 'assets/news.md', // TODO does not exist
-  /**
-   * Map containing the list of the aggregations and their displayed name.
-   * Should be kept in sync with the `FaidareAggregation` enum of the backend.
-   */
-  aggregationNames: {
-    node: 'Data provider',
-    species: 'Species',
-    annot: 'Ontology annotation',
-    hi: 'Holding institute',
-    bs: 'Biological status',
-    gn: 'Genetic nature',
-    coo: 'Country of origin',
-    tg: 'Taxon group'
-  },
-  // taxa links are used in germplasm cards
-  taxaLinks: {
-    NCBI: 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=',
-    ThePlantList: 'http://www.theplantlist.org/tpl1.1/record/',
-    TAXREF: 'https://inpn.mnhn.fr/espece/cd_nom/',
-    CatalogueOfLife: 'http://www.catalogueoflife.org/col/details/species/id/'
-  }
+  ...devEnvironment,
+  production: true
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
--- frontend/src/environments/environment.wheatis.prod.ts
+++ frontend/src/environments/environment.wheatis.prod.ts
@@ -1,35 +1,6 @@
-import { WheatisModule } from '../app/wheatis/wheatis.module';
+import { environment as devEnvironment } from './environment.wheatis';
 
 export const environment = {
-  production: true,
-  title: 'WheatIS - DataDiscovery',
-  navbar: {
-    title: 'Wheat Initiative',
-    logoUrl: 'http://www.wheatinitiative.org/',
-    links: [
-      { label: 'Home', url: './' },
-      { label: 'WheatIS website', url: 'http://wheatis.org/' },
-      { label: 'Wheat@URGI', url: 'https://wheat-urgi.versailles.inrae.fr/' }
-    ]
-  },
-  searchPlaceholder: 'Examples: yield, fhb',
-  resourceModule: WheatisModule,
-  helpMdFile: 'assets/help.md',
-  aboutUsMdFile: 'assets/about.md',
-  joinUsMdFile: 'assets/join.md',
-  legalMentionsMdFile: 'assets/legal.md',
-  eulaMdFile: 'assets/eula.md',
-  newsMdFile: 'assets/news.md',
-  dataProvider: 'Data providers',
-  /**
-   * Map containing the list of the aggregations and their displayed name.
-   * Should be kept in sync with the `WheatisAggregation` enum of the backend.
-   */
-  aggregationNames: {
-    entry: 'Data type',
-    db: 'Database',
-    node: 'Data provider',
-    species: 'Species',
-    annot: 'Ontology annotation'
-  }
+  ...devEnvironment,
+  production: true
 };
