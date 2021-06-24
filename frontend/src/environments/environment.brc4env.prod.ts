fd851da09eb60332a834a1c58c8dbfc95ec6f117
cexbrayat <cedric@ninja-squad.com>
2021-06-24 10:31:12 +0200

    refactor: share common config in env files

--- frontend/src/environments/environment.brc4env.prod.ts
+++ frontend/src/environments/environment.brc4env.prod.ts
@@ -1,32 +1,6 @@
-import { RareModule } from '../app/rare/rare.module';
+import { environment as devEnvironment } from './environment.brc4env';
 
 export const environment = {
-  production: true,
-  title: 'BRC4Env - Biological Resource Centre for the Environment',
-  navbar: {
-    title: 'BRC4Env Search',
-    logoUrl: '',
-    links: [{ label: 'BRC4Env Home', url: 'https://www.brc4env.fr/' }]
-  },
-  searchPlaceholder: 'e.g. salmo, chlamydomonas, phytomyzinae',
-  resourceModule: RareModule,
-  helpMdFile: 'assets/help.md',
-  aboutUsMdFile: 'assets/about.md',
-  joinUsMdFile: 'assets/join.md',
-  legalMentionsMdFile: 'assets/legal.md',
-  eulaMdFile: 'assets/eula.md',
-  newsMdFile: 'assets/news.md',
-  dataProvider: 'Data providers',
-  /**
-   * Map containing the list of the aggregations and their displayed name.
-   * Should be kept in sync with the `RareAggregation` enum of the backend.
-   */
-  aggregationNames: {
-    coo: "Pays d'origine",
-    coc: 'Pays de collecte',
-    domain: 'Domaine',
-    material: 'Matériel',
-    taxon: 'Taxon',
-    biotope: 'Biotope'
-  }
+  ...devEnvironment,
+  production: true
 };
--- frontend/src/environments/environment.rare.prod.ts
+++ frontend/src/environments/environment.rare.prod.ts
@@ -1,32 +1,6 @@
-import { RareModule } from '../app/rare/rare.module';
+import { environment as devEnvironment } from './environment.rare';
 
 export const environment = {
-  production: true,
-  title: 'RARe - Ressources Agronomiques pour la Recherche',
-  navbar: {
-    title: 'RARe Search',
-    logoUrl: '',
-    links: [{ label: 'AgroBRC-RARe Home', url: 'https://www.agrobrc-rare.org/' }]
-  },
-  searchPlaceholder: 'e.g. pisum sativum, rosa',
-  resourceModule: RareModule,
-  helpMdFile: 'assets/help.md',
-  aboutUsMdFile: 'assets/about.md',
-  joinUsMdFile: 'assets/join.md',
-  legalMentionsMdFile: 'assets/legal.md',
-  eulaMdFile: 'assets/eula.md',
-  newsMdFile: 'assets/news.md',
-  dataProvider: 'Data providers',
-  /**
-   * Map containing the list of the aggregations and their displayed name.
-   * Should be kept in sync with the `RareAggregation` enum of the backend.
-   */
-  aggregationNames: {
-    coo: "Pays d'origine",
-    coc: 'Pays de collecte',
-    domain: 'Domaine',
-    material: 'Matériel',
-    taxon: 'Taxon',
-    biotope: 'Biotope'
-  }
+  ...devEnvironment,
+  production: true
 };
