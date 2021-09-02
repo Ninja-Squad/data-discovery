0ec7f81b626b80053c672f77bb0e9726bed4fe54
cexbrayat <cedric@ninja-squad.com>
2021-09-02 15:45:41 +0200

    feat(faidare): footer
    Simple footer that displays the data providers and the Elixir logo.

---
+++ frontend/src/app/faidare/faidare-footer/faidare-footer.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+interface DataProvider {
+  name: string;
+  url: string;
+}
+
+@Component({
+  selector: 'dd-footer',
+  templateUrl: './faidare-footer.component.html',
+  styleUrls: ['./faidare-footer.component.scss']
+})
+export class FaidareFooterComponent {
+  dataProviders: Array<DataProvider> = [
+    {
+      name: 'URGI GnpIS',
+      url: 'https://urgi.versailles.inrae.fr/gnpis'
+    },
+    {
+      name: 'WUR EU-SOL BreeDB',
+      url: 'https://www.eu-sol.wur.nl'
+    },
+    {
+      name: 'VIB PIPPA',
+      url: 'http://pippa.psb.ugent.be'
+    },
+    {
+      name: 'IBET BioData',
+      url: 'https://biodata.pt'
+    },
+    {
+      name: 'NIB PISA',
+      url: 'http://www.pisa.nib.si'
+    },
+    {
+      name: 'CIRAD TropGENE',
+      url: 'http://tropgenedb.cirad.fr'
+    },
+    {
+      name: 'EBI European Nucleotide Archive',
+      url: 'https://www.ebi.ac.uk/eva'
+    },
+    {
+      name: 'TERRA-REF',
+      url: 'https://terraref.org'
+    }
+  ];
+}
