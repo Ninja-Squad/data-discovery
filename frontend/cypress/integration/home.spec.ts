5e8293b4f91f47654d18769ac10defdbf1fb77f3
jnizet <jb@ninja-squad.com>
2021-09-30 13:40:56 +0200

    fix: tests

--- frontend/cypress/integration/home.spec.ts
+++ frontend/cypress/integration/home.spec.ts
@@ -1,65 +1,63 @@
 describe('Home page', () => {
   it('should display aggregations', () => {
-    cy.intercept('GET', '/faidare-dev/api/aggregate?main=true', {
-      aggregations: [
-        {
-          name: 'tg',
-          type: 'LARGE',
-          buckets: [
-            {
-              key: 'NULL',
-              documentCount: 21416
-            }
-          ]
-        },
-        {
-          name: 'entry',
-          type: 'LARGE',
-          buckets: [
-            {
-              key: 'NULL',
-              documentCount: 10000
-            },
-            {
-              key: 'Genome annotation',
-              documentCount: 8090
-            },
-            {
-              key: 'Germplasm',
-              documentCount: 1432
-            }
-          ]
-        },
-        {
-          name: 'db',
-          type: 'LARGE',
-          buckets: [
-            {
-              key: 'GnpIS',
-              documentCount: 5963
-            },
-            {
-              key: 'CR-EST',
-              documentCount: 5387
-            }
-          ]
-        },
-        {
-          name: 'node',
-          type: 'SMALL',
-          buckets: [
-            {
-              key: 'IPK',
-              documentCount: 10000
-            },
-            {
-              key: 'INRAE-URGI',
-              documentCount: 9355
-            }
-          ]
-        }
-      ]
-    }).as('getMainAggregations');
+    cy.intercept('GET', '/faidare-dev/api/aggregate?main=true', [
+      {
+        name: 'tg',
+        type: 'LARGE',
+        buckets: [
+          {
+            key: 'NULL',
+            documentCount: 21416
+          }
+        ]
+      },
+      {
+        name: 'entry',
+        type: 'LARGE',
+        buckets: [
+          {
+            key: 'NULL',
+            documentCount: 10000
+          },
+          {
+            key: 'Genome annotation',
+            documentCount: 8090
+          },
+          {
+            key: 'Germplasm',
+            documentCount: 1432
+          }
+        ]
+      },
+      {
+        name: 'db',
+        type: 'LARGE',
+        buckets: [
+          {
+            key: 'GnpIS',
+            documentCount: 5963
+          },
+          {
+            key: 'CR-EST',
+            documentCount: 5387
+          }
+        ]
+      },
+      {
+        name: 'node',
+        type: 'SMALL',
+        buckets: [
+          {
+            key: 'IPK',
+            documentCount: 10000
+          },
+          {
+            key: 'INRAE-URGI',
+            documentCount: 9355
+          }
+        ]
+      }
+    ]).as('getMainAggregations');
     cy.visit('/');
     cy.wait('@getMainAggregations');
     cy.contains('h1', 'FAIR Data-finder for Agronomic REsearch');
