6807771d3d709ab2e5970e78051d37ee4262c8ee
cexbrayat <cedric@ninja-squad.com>
2023-11-16 17:59:38 +0100

    feat: use view transition

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { errorInterceptor } from './app/error-interceptor.service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -16,7 +16,8 @@ bootstrapApplication(AppComponent, {
         // when enabled, every click on an aggregation scrolls the page to the top, which is annoying
         scrollPositionRestoration: 'disabled',
         anchorScrolling: 'enabled'
-      })
+      }),
+      withViewTransitions({ skipInitialTransition: true })
     ),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideI18n()
