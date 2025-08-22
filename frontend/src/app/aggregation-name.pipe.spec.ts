131a67135489a36e3e5a497a845b080187a9cfaa
jnizet <jb@ninja-squad.com>
2025-08-22 08:19:55 +0200

    chore: upgrade to ngx-translate 17

--- frontend/src/app/aggregation-name.pipe.spec.ts
+++ frontend/src/app/aggregation-name.pipe.spec.ts
@@ -1,22 +1,32 @@
 import { AggregationNamePipe } from './aggregation-name.pipe';
 import { TestBed } from '@angular/core/testing';
-import { TranslateService } from '@ngx-translate/core';
+import { MissingTranslationHandler } from '@ngx-translate/core';
 import { provideI18nTesting } from './i18n/mock-18n.spec';
 
 describe('AggregationNamePipe', () => {
   let pipe: AggregationNamePipe;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [provideI18nTesting()] });
-    const translate = TestBed.inject(TranslateService);
     // override the missing translation handler for this test, as we want to check
     // what happens when the translation is not found (as it is when running the app)
-    translate.missingTranslationHandler = {
-      handle() {
+    const missingTranslationHandler: MissingTranslationHandler = {
+      handle(): undefined {
         // tests usually throws when a key is not found
         // here we do nothing
       }
     };
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideI18nTesting({
+          missingTranslationHandler: {
+            provide: MissingTranslationHandler,
+            useValue: missingTranslationHandler
+          }
+        })
+      ]
+    });
+
     pipe = TestBed.runInInjectionContext(() => new AggregationNamePipe());
   });
 
