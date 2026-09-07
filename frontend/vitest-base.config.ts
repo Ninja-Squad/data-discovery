d4a42eb7f5bfbd1fd5094526e38cc1821cfc92bf
cexbrayat <cedric@ninja-squad.com>
2026-09-07 21:32:39 +0200

    test: externalize marked-katex-extension in Vitest

--- frontend/vitest-base.config.ts
+++ frontend/vitest-base.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 const timeout = process.env.CI ? 5000 : 2000;
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['marked-katex-extension']
+  },
   test: {
     expect: {
       poll: {
